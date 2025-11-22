db5229841766615c13c9a6789b1ed3b09264eefa
A-coder152 <abdullahalsaleem19@gmail.com>
2025-11-21 20:38:30 -0500

    ultra basic task splittin

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const bigTasksDiv = document.getElementById("bigTasksDiv")
 const newTaskInput = document.getElementById("newTaskInput")
 const newTaskBtn = document.getElementById("newTaskBtn")
+const splitTasksDiv = document.getElementById("splitTasksDiv")
+const splitTasksBtn = document.getElementById("splitTasksBtn")
 
 newTaskBtn.addEventListener("click", () => {
     const newBigTask = document.createElement("div")
@@ -9,4 +11,12 @@ newTaskBtn.addEventListener("click", () => {
     <p>woah look its a new task</p>`
     newBigTask.classList.add("bigTask")
     bigTasksDiv.appendChild(newBigTask)
+})
+
+splitTasksBtn.addEventListener("click", () => {
+    const newSplitTask = document.createElement("div")
+    newSplitTask.innerHTML = `
+    <h5> split task title </h5>
+    <p> split task description </p>`
+    splitTasksDiv.appendChild(newSplitTask)
 })
